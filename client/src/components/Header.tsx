1e93c4cb562314420509b7ea6117daac29dd46ca
João Reis <joaoreisjpk@gmail.com>
2022-03-05 19:28:13 -0300

    finishing the header styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Link from './Link';
+import NextLink from 'next/link';
 
 export default function Header() {
   return (
     <Navbar bg='dark' variant='dark' expand='lg'>
       <Container className='d-flex justify-content-between'>
-        <Navbar.Brand href='#home'>TrybeSocial</Navbar.Brand>
+        <NextLink href='/main-page' passHref>
+          <Navbar.Brand href='#'>TrybeSocial</Navbar.Brand>
+        </NextLink>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav flex-grow-0'>
           <Nav className='me-auto'>
