0fed006b2ab809300d4f709f3ec6cfc81ac9f091
João Reis <joaoreisjpk@gmail.com>
2023-01-09 15:31:18 -0300

    feat(CircleCI): adding config

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log('server online on port 3333'));
+const { PORT, DB_URL, JWT_SECRET } = process.env;
+app.listen(PORT, () => {
+  console.log(`server online on port ${PORT}`);
+  console.log('DB_URL', DB_URL);
+  console.log('JWT_SECRET', JWT_SECRET);
+});
